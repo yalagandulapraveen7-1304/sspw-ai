0dc24224203b335c299fc0efe34e370faef559cd
Praveen Yalagandula <yalagandulapraveen7@gmail.com>
2026-08-28 20:15:47 +0530

    latest update

---
+++ app.py
@@ -0,0 +1,7 @@
+﻿import os
+import sys
+import runpy
+
+if __name__ == '__main__':
+    src_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'src', 'app.py')
+    runpy.run_path(src_file, run_name='__main__')
--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from flask_basicauth import BasicAuth # 1. Import the security tool
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 CORS(app)
 
 # 2. Set the exact username and password for SSPW
@@ -80,4 +81,4 @@ def cost_estimator():
 # This allows local testing but correctly binds for Render in production
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
